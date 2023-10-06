e33c397b8b9e73e5542ec780ac71e1b593cb1654
Misha Flyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-10-07 00:28:31 +0300

    add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import { FaBook } from 'react-icons/fa';
 import css from './Home.module.css';
 
 export default function Home() {
   return (
     <div className={css.container}>
-      <h1 className={css.title}>Your PhoneBook</h1>
+      <h1 className={css.title}>
+        Your PhoneBook <FaBook color=" blueviolet" size={65} />{' '}
+      </h1>
     </div>
   );
 }
+// FaBook;
