241e69b946bc3c96ab3cb1a46cb9a9df44a7c8f7
MishaFlyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-10-07 07:19:15 +0000

    Deploying to gh-pages from @ MishaFlyunt/goit-react-hw-08-phonebook@35fdbcb6bc98dd1af388a97f7a01c4f30c6d6bc7 🚀

---
+++ static/js/936.ac59aacb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{4936:function(e,t,o){o.r(t),o.d(t,{default:function(){return a}});var r=o(6355),n="Home_container__w+u63",c="Home_title__L087v",s=o(184);function a(){return(0,s.jsx)("div",{className:n,children:(0,s.jsxs)("h1",{className:c,children:["Your PhoneBook ",(0,s.jsx)(r.Mp$,{color:" blueviolet",size:75})," "]})})}}}]);
+//# sourceMappingURL=936.ac59aacb.chunk.js.map
